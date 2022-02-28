7f75bb2ba1f577484f4584cc9df51db02b5eb33d
Artem Kartavtsev <a.kartavtsev@liga-team.ru>
2022-02-28 16:38:46 +0300

    Меняет эмодзи галочек

--- source/js/main.js
+++ source/js/main.js
@@ -32,7 +32,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
 // вместо модификаторов .block--active используем утилитарные классы
 // .is-active || .is-open || .is-invalid и прочие (обязателен нейминг в два слова)
-// .select.select--opened ❌ ---> [data-select].is-open ✔️
+// .select.select--opened ❌ ---> [data-select].is-open ✅
 
 // выносим все в дата атрибуты
 // url до иконок пинов карты, настройки автопрокрутки слайдера, url к json и т.д.
